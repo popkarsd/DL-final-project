d10759ddc4d6ff14091630e7f9587dbd965f1b2c
Balint Rozsa <rozsabalint256b@gmail.com>
2025-12-06 22:26:28 -0500

    Don't track data directory

--- src/gen.sh
+++ src/gen.sh
@@ -1,7 +1,10 @@
-g++ gen.cpp -O2 -std=c++20 -o gen
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+echo $parent_path
+
+g++ $parent_path/gen.cpp -O2 -std=c++20 -o $parent_path/gen
 
 for i in {1..100}
 do
     echo $i
-    ./gen $i >../data/$i.in
+    $parent_path//gen $i >$parent_path/../data/$i.in
 done
